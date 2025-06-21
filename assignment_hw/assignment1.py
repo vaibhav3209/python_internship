95909b0e16d08790c62d3f103c0705fe0ee8e26b
vaibhav3209 <vaibhavmalav35@gmail.com>
2025-06-21 17:29:57 +0530

    initial upload

---
+++ assignment_hw/assignment1.py
@@ -0,0 +1,35 @@
+# #  .............QUESTION1...........
+# from itertools import count
+#
+# name=input("enter srtudent name")
+# clas=input("enter srtudent class")
+# a,b,c,d,e = (input("enter marks of students").split())
+# total_marks = int(a)+int(b)+int(c)+int(d)+int(e)
+# per= total_marks/5
+# print(f"{name} of {clas} got {total_marks}marks and {per:.2F}% ")
+#
+# print(type(per))  #it is float
+#
+#
+# #  .............QUESTION2...........
+#
+# str1 = input("enter string 1:")
+# str2 = input("enter string 2:")
+#
+#
+# z= str1+" "+str2+"\n"
+# print(z)
+# seq = ("my name is:")   #har letter k baad string paste karra
+# seq = ("my name is:"," ")
+#
+# print(z.join(seq))
+# print(z.splitlines())
+#
+# print(z.title())
+# print(z.capitalize())
+# print(z.swapcase())
+# ..........................................................
+
+
+
+
---
+++ assignment_hw/assignment2.py
@@ -0,0 +1,71 @@
+#QUESTION 2.............
+
+
+#
+# name=input("enter srtudent name")
+# clas=input("enter srtudent class")
+# a,b,c,d,e = (input("enter marks of students").split())
+# total_marks = int(a)+int(b)+int(c)+int(d)+int(e)
+# per= total_marks/5
+# print(f"{name} of {clas} got {total_marks}marks and {per:.2F}% ")
+#
+#
+# if per>60:
+#     print("grade A")
+# elif per>50 :
+#     print("grade B")
+# elif per > 40 :
+#     print("grade C")
+# else :
+#     print("fail")
+#     print("we are sorry")
+# print(type(per))
+
+
+# QUESTION 3..................
+# x=int(input("wnter a number"))
+# fact=1
+# for x in range(1,x+1):
+#     fact*=x
+# print(fact)
+#..................................
+
+
+#QUESTION 4 ................
+list=[]                         #ITEMS TO BE ADDED
+sum=0                           #TOTAL TO BE CALCULATED
+while True:
+    print(''' 
+1. Create Bill
+2. Display Item Price and total bill amount
+3. Display Total
+4. Exit
+''')
+    ch= int(input("Enter your choice "))
+    if ch == 1:
+       # pass
+       print("enter number  order items ")               #do by walris operator
+       item=input()
+       print(f"enter name and amount of{item} items")
+       name = input()
+       amount=int(input())
+       list.extend()
+
+    elif ch == 2:
+       pass
+    elif ch == 3:
+       pass
+    elif ch == 4:
+       break
+    else:
+        print("invailde inputia")
+
+
+
+
+
+
+
+
+
+
